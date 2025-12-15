f4fb6ee2aa9ab7be1c2f1899b0a920f8b577d41f
拓輝 河村 <smarf.tech18@gmail.com>
2025-12-15 14:01:52 +0000

    git init

---
+++ srv/src/main/java/customer/omfr_allocation/Application.java
@@ -0,0 +1,13 @@
+package customer.omfr_allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
